1555a08500124654f28c71aa8284ccbd69761f15
incredible <nitishjha913@gmail.com>
2023-06-10 18:33:32 +0000

    Update 8 files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience.jsx";
+import Projects from "./components/Projects";
 function App() {
   return (
     <div className="app">
@@ -11,6 +12,7 @@ function App() {
         <Hero />
         <About />
         <Experience />
+        <Projects />
       </div>
     </div>
   );
